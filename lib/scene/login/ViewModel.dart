e78580219063fe72769b45a9d0cd7990f6967c5f
Ngọc Trinh <igg.anhkiet1@gmail.com>
2023-09-26 02:04:19 +0700

    add login

--- lib/scene/login/ViewModel.dart
+++ lib/scene/login/ViewModel.dart
@@ -1,6 +1,9 @@
 part of './LoginView.dart';
 
 class _ViewModel with ChangeNotifier {
+
+  _ViewModel(this.context);
+  final BuildContext context;
   final _authService = AuthService.shared;
   final _localStorage = LocalStorageService.shared;
   var _isLoading = false;
@@ -28,15 +31,22 @@ class _ViewModel with ChangeNotifier {
   login({required String email, required String password}) async {
     isLoading = true;
     _authService.login(email: email, password: password)
-      .then((value) {
+      .then((value) async {
+        if(!(value.status ?? false)) {
+          context.showCustomSnackBar(value.message);
+          return;
+        }
         user = value.user;
-        Future.wait([
+        await Future.wait([
           _localStorage.saveValue(key: LocalStorageKey.jwtKey, value: value.token),
           _localStorage.saveObject(key: LocalStorageKey.user, object: user)
-        ]).then((value) => presentController.hidePresent());
+        ]).then((value) {
+          context.showCustomSnackBar('Đã đăng nhập với ${user?.email ?? ''}');
+          presentController.hidePresent();
+        });
       })
       .onError((ErrorResponse error, stackTrace) {
-        print(error.error);
+        context.showCustomSnackBar(error.error);
       })
       .whenComplete(() {
         isLoading = false;
